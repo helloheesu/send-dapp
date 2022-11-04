162271b694a5214974cf0b4f37d34f0e972f63d7
Heesu Jung <heesu.jung.developer@gmail.com>
2022-11-04 13:06:17 +0900

    refactor: change code order

--- stores.ts
+++ stores.ts
@@ -2,24 +2,28 @@ import create from "zustand";
 
 interface SendStore {
   senderMnemonic: string;
+  setSenderMnemonic: (mnemonic: string) => void;
+
   receiverAddress: string;
+  setReceiverAddress: (address: string) => void;
+
   token: {
     denom: string;
     amount: number;
   };
-  setSenderMnemonic: (mnemonic: string) => void;
-  setReceiverAddress: (address: string) => void;
   setToken: (token: { denom: string; amount: number }) => void;
 }
 
 export const useSendStore = create<SendStore>()((set) => ({
   senderMnemonic: "",
+  setSenderMnemonic: (mnemonic: string) => set({ senderMnemonic: mnemonic }),
+
   receiverAddress: "",
+  setReceiverAddress: (address: string) => set({ receiverAddress: address }),
+
   token: {
     denom: "",
     amount: 0,
   },
-  setSenderMnemonic: (mnemonic: string) => set({ senderMnemonic: mnemonic }),
-  setReceiverAddress: (address: string) => set({ receiverAddress: address }),
   setToken: (token: { denom: string; amount: number }) => set({ token: token }),
 }));
